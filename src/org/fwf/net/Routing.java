1c9ac4dfc5e8c70d786961bd5d7851f576005afe
G. Medina <github@g-medina.de>
2019-04-19 12:51:32 +0200

    Add main routing class

---
+++ src/org/fwf/net/Routing.java
@@ -0,0 +1,92 @@
+package org.fwf.net;
+
+import com.sun.net.httpserver.HttpHandler;
+import org.fwf.log.Logger;
+import org.fwf.log.Severity;
+import org.fwf.mvc.Controller;
+import org.fwf.obj.ClassRegister;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+class Routing {
+
+    private static HashMap<String, HttpHandler> routeHandlerMap;
+
+    static Map<String, HttpHandler> getRouteHandlerMap() {
+        if (routeHandlerMap == null) {
+            routeHandlerMap = new HashMap<>();
+            fillRouteHandlerMap();
+        }
+        return routeHandlerMap;
+    }
+
+    private static void fillRouteHandlerMap() {
+        Set<Controller> controllerInstances = ClassRegister.getInstance().getControllerInstances();
+        for (Controller controller : controllerInstances) {
+
+            Class<? extends Controller> controllerClass = controller.getClass();
+            String baseRouteValue = retrieveControllerBaseRoute(controllerClass);
+
+            // check if base route is valid
+            if (baseRouteValue == null) {
+                Logger.log(Severity.WARNING, "Invalid route for controller " +
+                        controllerClass.getCanonicalName());
+                continue;
+            }
+
+            Method[] actions = controllerClass.getDeclaredMethods();
+            for (Method action : actions) {
+
+                String route = retrieveControllerMethodRoute(baseRouteValue, action);
+
+                // check if route is valid
+                if (route == null) {
+                    Logger.log(Severity.WARNING, "Invalid route for controller action" +
+                            controllerClass.getCanonicalName() + "->" + action.getName());
+                    continue;
+                }
+
+                // check if route is already registered
+                if (routeHandlerMap.containsKey(route)) {
+                    Logger.log(Severity.WARNING, "Cannot register action " +
+                            controllerClass.getCanonicalName() + "->" + action.getName() +
+                            " .Route '" + route + "' is already registered");
+                    continue;
+                }
+
+                routeHandlerMap.put(route, httpExchange -> {
+                    invokeControllerAction(controller, action);
+                    httpExchange.close();
+                });
+            }
+        }
+    }
+
+    private static String retrieveControllerBaseRoute(Class<? extends Controller> controllerClass) {
+        if (controllerClass.isAnnotationPresent(BaseRoute.class)) {
+            return controllerClass.getAnnotation(BaseRoute.class).value();
+        } else {
+            return null;
+        }
+    }
+
+    private static String retrieveControllerMethodRoute(String baseRouteValue, Method declaredMethod) {
+        if (declaredMethod.isAnnotationPresent(Route.class)) {
+            return baseRouteValue + declaredMethod.getAnnotation(Route.class).value();
+        } else {
+            return null;
+        }
+    }
+
+    private static void invokeControllerAction(Controller controller, Method declaredMethod) {
+        try {
+            declaredMethod.invoke(controller);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            Logger.log(Severity.ERROR, e.getMessage(), e);
+        }
+    }
+}
