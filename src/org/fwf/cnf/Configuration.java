2ca1faa5b1a22351360d0ae9f4383dabbf2b4040
G. Medina <github@g-medina.de>
2019-04-19 17:49:43 +0200

    Add configuration properties for db and logging

--- src/org/fwf/cnf/Configuration.java
+++ src/org/fwf/cnf/Configuration.java
@@ -24,6 +24,18 @@ public class Configuration {
         return properties.getProperty(key);
     }
 
-    public static final String BASE_PACKAGE = "basePackage";
+    public static final String APP_PACKAGE = "app.package";
+    public static final String APP_PORT = "app.port";
+    public static final String DB_DRIVER = "db.driver";
+    public static final String DB_HOSTNAME = "db.hostname";
+    public static final String DB_PORT = "db.port";
+    public static final String DB_NAME = "db.name";
+    public static final String DB_USERNAME = "db.username";
+    public static final String DB_PASSWORD = "db.password";
+    public static final String LOG_INFO = "log.info";
+    public static final String LOG_DEBUG = "log.debug";
+    public static final String LOG_WARNING = "log.warning";
+    public static final String LOG_ERROR = "log.error";
+    public static final String LOG_FILE = "log.error";
 
 }
