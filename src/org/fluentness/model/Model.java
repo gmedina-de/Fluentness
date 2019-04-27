562ea85c85f7b8e3c5737e0f158b9929279758be
G. Medina <github@g-medina.de>
2019-04-27 19:18:38 +0200

    Add default methods for create, delete, update to model

--- src/org/fluentness/model/Model.java
+++ src/org/fluentness/model/Model.java
@@ -1,5 +1,18 @@
 package org.fluentness.model;
 
+import org.fluentness.repository.RepositoryImpl;
+
 public interface Model {
 
+    default int create() {
+        return RepositoryImpl.create(this, this.getClass());
+    }
+
+    default int update() {
+        return RepositoryImpl.update(this, this.getClass());
+    }
+
+    default int delete() {
+        return RepositoryImpl.delete(this, this.getClass());
+    }
 }
