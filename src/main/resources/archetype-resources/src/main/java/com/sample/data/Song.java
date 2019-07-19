313830045231a6920e5ce2e77da594243388ab66
G. Medina <github@g-medina.de>
2019-07-19 22:32:53 +0200

    Use separate directory for sample sources

---
+++ src/main/resources/archetype-resources/src/main/java/com/sample/data/Song.java
@@ -0,0 +1,47 @@
+package com.sample.data;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "song")
+public class Song {
+
+    private int id;
+    private String title;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "title")
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String name) {
+        this.title = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Song song = (Song) o;
+        return Objects.equals(id, song.id) &&
+            Objects.equals(title, song.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title);
+    }
+}
---
+++ src/main/resources/archetype-resources/src/main/java/com/sample/flow/Views.java
@@ -0,0 +1,80 @@
+package com.sample.flow;
+
+import com.sample.data.Song;
+import org.fluentness.flow.form.FormConsumer;
+import org.fluentness.flow.style.StyleConsumer;
+import org.fluentness.flow.view.View;
+import org.fluentness.flow.view.ViewProvider;
+
+public class Views extends ViewProvider implements StyleConsumer<Styles>, FormConsumer<Forms> {
+
+    View base(View toInclude) {
+        return html(
+            head(
+                title("A music archive made with love and Fluentness"),
+                meta(NAME -> "lang", CONTENT -> "en"),
+                meta(CHARSET -> "utf-8"),
+                style(styles().bundle),
+                includeJs("script.min.js")
+            ),
+            body(
+                div(attrs(CLASS -> "container"),
+                    h1(translate("welcome_message")),
+                    toInclude
+                )
+            )
+        );
+    }
+
+    View createSong = base(
+        div(attrs(CLASS -> "row"),
+            div(attrs(CLASS -> "column"),
+                h2(translate("song_create")),
+                forms().createSong
+            )
+        )
+    );
+
+    View songList = base(
+        div(attrs(CLASS -> "row"),
+            div(attrs(CLASS -> "column"),
+                div(attrs(CLASS -> "row"),
+                    div(attrs(CLASS -> "column column-50"),
+                        h2(translate("song_list"))
+                    ),
+                    div(attrs(CLASS -> "column column-50"),
+                        forms().searchSong
+                    )
+                ),
+                table(
+                    thead(
+                        tr(
+                            th(translate("song_title")),
+                            th(translate("song_artist")),
+                            th(translate("song_album")),
+                            th(translate("song_year")),
+                            th(translate("song_is_new")),
+                            th(translate("song_update")),
+                            th(translate("song_delete"))
+                        )
+                    ),
+                    tbody(
+                        forEachItemIn("songs", Song.class,
+                            song -> tr(
+                                td(song.getTitle()),
+                                td(print("testParameter"))
+//                                td(song.getBoolean("is_new") ? "✔" : "\uD83D\uDDD9"),
+//                                td(a(attrs(CLASS -> "button", HREF -> "/song/update/" + song.getId()), "\uD83D\uDD89")),
+//                                td(a(attrs(CLASS -> "button", HREF -> "/song/delete/" + song.getId()), "\uD83D\uDDD1"))
+                            )
+                        )
+                    )
+                ),
+                a(attrs(CLASS -> "button", href -> "/song/create"),
+                    translate("song_create")
+                )
+            )
+        )
+    );
+
+}
