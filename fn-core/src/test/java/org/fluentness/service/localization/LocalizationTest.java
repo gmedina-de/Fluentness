7eadc469e8a33943bc2ed298e9c791d7607aa516
G. Medina <github@g-medina.de>
2020-06-06 10:19:37 +0200

    Add LocalizationTest

---
+++ fn-core/src/test/java/org/fluentness/service/localization/LocalizationTest.java
@@ -0,0 +1,16 @@
+package org.fluentness.service.localization;
+
+import org.fluentness.service.configuration.Setting;
+
+public class LocalizationTest {
+
+    private final Setting<String> stringSetting = new Setting<>("defaultValue");
+    private final Setting<Integer> integerSetting = new Setting<>();
+    private final Localization localization = new AbstractLocalization() {
+
+    };
+
+
+
+
+}
